b826a09e96a647c42d75ac73becdd14adbba1d49
LukaJuric1998 <juricluka783@gmail.com>
2025-05-22 16:03:34 +0200

    promjenaimena

--- Webshop/HTML/darkmode.js
+++ Webshop/HTML/darkmode.js
@@ -1,9 +1,15 @@
+
+
 function toggleDarkMode() {
   // Promjena teme (npr. dodavanje/uklanjanje klase)
   document.body.classList.toggle("dark");
 
-  
   const icon = document.getElementById("theme-icon");
+  const isDark = document.documentElement.classList.toggle("dark");
+
+   localStorage.setItem("theme", isDark ? "dark" : "light");
+
+  
 
   // Promijeni ikonu
   if (icon.classList.contains("fa-moon")) {
@@ -14,3 +20,18 @@ function toggleDarkMode() {
     icon.classList.add("fa-moon");
   }
 }
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  const icon = document.getElementById("theme-icon");
+  const savedTheme = localStorage.getItem("theme");
+
+  if (icon) {
+    if (savedTheme === "dark") {
+      icon.classList.add("fa-sun");
+    } else {
+      icon.classList.add("fa-moon");
+    }
+  }
+});
